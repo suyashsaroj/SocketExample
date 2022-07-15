a587adeeda8ae73ef3541ed3b546760191f2d476
suyashsaroj <48505375+suyashsaroj@users.noreply.github.com>
2022-07-16 02:23:40 +0530

    new files

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require('path');
+app.use(express.static(path.join(__dirname + '/index.html')));
+var cors = require('cors');
+app.use(cors());
